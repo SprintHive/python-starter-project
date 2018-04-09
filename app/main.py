ef62c095d842324c3af284f9bfe7aece416c9200
Jonathan Zwart <jonathan.zwart@sprinthive.com>
2018-04-09 11:24:10 +0200

    Added TestImports method and some comments

--- app/main.py
+++ app/main.py
@@ -1,14 +1,33 @@
+# SprintHive Pty Ltd
+# Python Starter Project ('ground-zero')
+
+# Contact details
+# Jonathan Zwart <jz@sprinthive.com>
+# Monday 9 April 2018
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<span style='color:red'>I am app 1</span>"
+    return '<span style='color:red'>I am app 1</span>'
 
-@app.route("/hellofromflask")
+@app.route('/hellofromflask')
 def hello():
-    return "Hello World from Flask"
+    return 'Hello World from Flask'
+
+@app.route('/TestImports')
+def testImports():
+
+    print 'Hello world'
+    try:
+        from flask import Flask
+        return 'Imports worked'
+    except:
+        return 'Imports failed'
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
+    # Port is handled by UWSGI internally
     app.run(host='0.0.0.0', debug=True, port=1492)
