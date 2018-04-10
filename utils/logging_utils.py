c7968a7fdee2f39c923f5b11427711edf632cbf5
Jonathan Zwart <jonathan.zwart@sprinthive.com>
2018-04-10 08:36:34 +0200

    Fixed quotes in default format and added an INFO

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 def setup_logging(
     default_path='logging.yml',
     default_level=logging.INFO,
-    default_format="%(levelname)-7.7s] [%(asctime)s] [%(funcName)20s()] L%(lineno)d %(message)s",
+    default_format='%(levelname)-7.7s] [%(asctime)s] [%(funcName)20s()] L%(lineno)d %(message)s',
     env_key='LOG_CFG'
 ):
     """
@@ -31,6 +31,7 @@ def test_logging():
     :return:
     """
 
+    logging.info('Logging enabled in %s' % __name__)
     logging.info('TEST: INFO')
     logging.warning('TEST: WARNING')
     logging.error('TEST: ERROR')
